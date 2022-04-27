06883f10f9399368a9ac201b213436d611b666c8
Tom Scott <derpy4me@gmail.com>
2022-04-26 20:59:04 -0600

    minor tweaks and better reactive centering.

--- scripts/portal.js
+++ scripts/portal.js
@@ -4,3 +4,6 @@ const lastModifiedElement = document.getElementById("lastUpdated");
 
 lastModifiedElement.innerHTML = `Last Updated: ${lastModifiedDate.toLocaleDateString("en-US")} 
 ${lastModifiedDate.toLocaleTimeString("en-US")}`;
+
+// `Last Updated: ${document.lastModified}`;
+// This returns a 24 hour format on my machine.
